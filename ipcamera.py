b00826f34930554e0fb8ee0a56899dc641c80b68
David Ren <823762837@qq.com>
2018-11-07 14:43:39 +0800

    the ip cam on pepper's head
    a little test script on ip cam.

---
+++ ipcamera.py
@@ -0,0 +1,28 @@
+import cv2
+import numpy
+cap = cv2.VideoCapture("rtsp://192.168.3.53:6554/stream_0")
+
+
+print cap.isOpened()
+
+
+
+frameNum = 1
+
+while(cap.isOpened()):
+
+    ret,frame = cap.read()
+
+    print frameNum
+
+    frameNum = frameNum + 1
+
+    cv2.imshow('frame',frame)
+
+    if cv2.waitKey(1) == ord('q'):
+
+        break
+
+cap.release()
+
+cv2.destroyAllWindows()
